4dea2afa0f34d0d108979b4786b6eb6127727550
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-20 14:57:20 +0100

    feat: add header with conditional styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,9 +2,19 @@ interface NavbarProps {
   variant: 'transparent' | 'solid'
 }
 
+const navLink = [
+  'Home',
+  'Properties',
+  'MarketPlace'
+]
+
 const Navbar = ( { variant = 'transparent' }:NavbarProps ) => {
+  const isTransparent = variant === 'transparent'
+
   return (
-    <div>Navbar</div>
+    <header className={ `w-full top-0 left-0 z-50 ${ isTransparent ? 'absolute' : 'bg-card sticky border-b border-black/5' }` }>
+      Navbar
+    </header>
   )
 }
 
