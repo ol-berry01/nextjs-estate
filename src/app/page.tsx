ca7d43de51c91147f3de166e09712d4656418b35
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-20 11:40:07 +0100

    feat: set hero background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image'
+
 import FrontendLayout from '@/components/layouts/FrontendLayout'
 
 const Home = () => {
   return (
-    <FrontendLayout> 
+    <FrontendLayout>
+      <section>
+        <Image
+          src={ '/images/hero.jpg' }
+          alt={ 'Background hero image' }
+          fill
+          className={ 'object-center object-cover absolute inset-0' }
+        />
+        <div className={ 'min-h-screen pt-32 lg:lt-36 py-2 relative flex items-center overflow-hidden' }>
+        </div>
+      </section>
     </FrontendLayout>
   )
 }
