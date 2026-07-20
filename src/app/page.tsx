062753c52a4d2333df511b57193ed7076735197d
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-20 14:35:07 +0100

    feat: add hero bg overlay
    include section annotations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,18 @@ const Home = () => {
   return (
     <FrontendLayout>
       <section>
+        {/* bg image */}
         <Image
           src={ '/images/hero.jpg' }
           alt={ 'Background hero image' }
           fill
           className={ 'object-center object-cover absolute inset-0' }
         />
+
+        {/* overlay */}
+        <div className={ 'bg-black/35 absolute inset-0' }></div>
+
+        {/* content */}
         <div className={ 'min-h-screen pt-32 lg:lt-36 py-2 relative flex items-center overflow-hidden' }>
         </div>
       </section>
