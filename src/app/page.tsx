dc41c89a9b3fe01c13688e44a56928cc0ab5dc4e
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-20 14:40:19 +0100

    feat: set hero overlay gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,9 @@ const Home = () => {
         {/* overlay */}
         <div className={ 'bg-black/35 absolute inset-0' }></div>
 
+        {/* gradient */}
+        <div className="bg-linear-to-r from-slate-950/50 via-slate-900/40 to-transparent absolute inset-0"></div>
+
         {/* content */}
         <div className={ 'min-h-screen pt-32 lg:lt-36 py-2 relative flex items-center overflow-hidden' }>
         </div>
