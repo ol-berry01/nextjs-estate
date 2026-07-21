36e2502b2f26cd63612b6987013a4ddbbc2e72ac
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-21 13:18:49 +0100

    feat: handle left and right positioned icons

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
-import { ReactNode, 
-  ButtonHTMLAttributes } from 'react'
+import { ReactNode, ButtonHTMLAttributes } from 'react'
+import clsx from 'clsx'
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & { 
   variant?: 'primary' | 'outline',
   icon?: ReactNode,
   iconPosition?: 'left' | 'right',
@@ -9,7 +9,7 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   loading?: false
 }
 
-const Button = ( { 
+const Button = ( {  
   variant = 'primary', 
   children, 
   icon, 
@@ -19,10 +19,44 @@ const Button = ( {
   disabled, 
   className, 
   ...props 
-}:ButtonProps ) => {
-  return (
-    <div>Button</div>
-  )
-}
+}:ButtonProps ) => (
+  <button
+    disabled={ loading || disabled }
+    className={ clsx( 
+      `
+        h-13 px-6 rounded-2xl
+        font-semibold cursor-pointer transition-all duration-300
+        inline-flex items-center justify-center gap-2
+      `,
+      { 
+        'bg-primary hover:bg-primary/90 text-white': variant === 'primary',
+        'bg-card text-text border border-black/10 hover:text-primary hover:border-primary': variant = 'outline',
+        'w-full': fullWidth,
+        'opacity-70 cursor-not-allowed': loading || disabled
+      },
+      className
+    ) }
+    { ...props }
+  >
+    { loading ? (
+      <>
+        <div className={ 'w-4 h-4 border-2 border-current border-t-transparent rounded-full animate-spin' } />
+        <span>Loading...</span>
+      </>
+    ) : (
+      <>
+        { /* icon position: left */ }
+        { icon && iconPosition === 'left' && (
+          <span>{ icon }</span>
+        ) }
+        { children}
+        { /* icon position right */ }
+        { icon && iconPosition === 'right' && (
+          <span>{ icon }</span>
+        ) }
+      </>
+    ) }
+  </button>
+)
 
 export default Button
