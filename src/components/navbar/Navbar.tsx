f4e4e0aa2ef30f69cc36e9108b2d706d63eaa74a
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-21 10:14:27 +0100

    feat: dynamically output nav links

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -17,6 +17,7 @@ const Navbar = ( { variant = 'transparent' }:NavbarProps ) => {
     <header className={ `w-full top-0 left-0 z-50 ${ isTransparent ? 'absolute' : 'bg-card sticky border-b border-black/5' }` }>
       <div className={ 'max-w-7xl mx-auto px-6 lg:px-12' }>
         <nav className={ `h-20 flex justify-between items-center ${ isTransparent ? 'bg-white/5 mt-6 px-6 border border-white/10 rounded-3xl backdrop-blur-2xl' : 'px-0' }` }>
+          {/* logo */}
           <Link
             href={ '/' }
             className={ 'text-2xl font-semibold flex items-center' }
@@ -28,6 +29,19 @@ const Navbar = ( { variant = 'transparent' }:NavbarProps ) => {
               Estate
             </span>
           </Link>
+
+          {/* desktop links */}
+          <div className={ 'hidden lg:flex items-cetner gap-8' }>
+            { navLink.map( ( item ) => (
+              <Link
+                href={ item === 'Home' ? '/' : `${ item.toLowerCase() }` }
+                className={ `text-sm hover:text-primary font-medium transition ${ isTransparent ? 'text-white/80' : 'text-text/70' }` }
+                key={ item }
+              >
+                { item }
+              </Link>
+            ) )}
+          </div>
         </nav>
       </div>
     </header>
