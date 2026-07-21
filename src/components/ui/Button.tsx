2cf86bc2f1d45702274681d2b451488ca21b291a
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-21 12:35:07 +0100

    feat: set available button props
    add optional loading prop

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,13 +1,25 @@
-import { ReactNode, ButtonHTMLAttributes } from 'react'
+import { ReactNode, 
+  ButtonHTMLAttributes } from 'react'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: 'primary' | 'outline',
   icon?: ReactNode,
   iconPosition?: 'left' | 'right',
-  fullWidth?: boolean
+  fullWidth?: boolean,
+  loading?: false
 }
 
-const Button = ( { variant }:ButtonProps ) => {
+const Button = ( { 
+  variant = 'primary', 
+  children, 
+  icon, 
+  iconPosition, 
+  fullWidth = false, 
+  loading = false, 
+  disabled, 
+  className, 
+  ...props 
+}:ButtonProps ) => {
   return (
     <div>Button</div>
   )
