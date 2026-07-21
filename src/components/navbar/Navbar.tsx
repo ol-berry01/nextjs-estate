93cb7020cd9269f35a858adb28734049880f1f74
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-21 09:42:48 +0100

    feat: add nav element
    include condition style rules when isTransparent

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 interface NavbarProps {
-  variant: 'transparent' | 'solid'
+  variant?: 'transparent' | 'solid'
 }
 
 const navLink = [
@@ -13,7 +13,11 @@ const Navbar = ( { variant = 'transparent' }:NavbarProps ) => {
 
   return (
     <header className={ `w-full top-0 left-0 z-50 ${ isTransparent ? 'absolute' : 'bg-card sticky border-b border-black/5' }` }>
-      Navbar
+      <div className={ 'max-w-7xl mx-auto px-6 lg:px-12' }>
+        <nav className={ `h-20 flex justify-between items-center ${ isTransparent ? 'bg-white/5 mt-6 px-6 border border-white/10 rounded-3xl backdrop-blur-2xl' : 'px-0' }` }>
+
+        </nav>
+      </div>
     </header>
   )
 }
