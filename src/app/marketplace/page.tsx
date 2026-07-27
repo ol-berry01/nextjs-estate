932c880c8611817ee7f3d528f84fef13bb71dd90
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-27 16:31:46 +0100

    feat: add marketplace page header

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -9,6 +9,16 @@ const MarketPlace = () => {
       <Navbar 
         variant={ 'solid' }
       />
+
+      <div className="w-full max-w-7xl mx-auto p-6 lg:px-12">
+        <div className="flex justify-between">
+          <h2 className="text-text text-2xl font-bold md:text-3xl">
+            Explore
+          </h2>
+
+          <Button>Filter</Button>
+        </div>
+      </div>
     </FrontendLayout>
   )
 }
