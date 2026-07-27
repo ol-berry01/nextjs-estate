6d32b664008024b72566374d62e8c91690f05aa5
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-27 16:31:24 +0100

    feat: create marketplace page
    import FrontendLayout and Navbar with solid variant

---
+++ src/app/marketplace/page.tsx
@@ -0,0 +1,16 @@
+import FrontendLayout from '@/components/layouts/FrontendLayout'
+import { Navbar } from '@/components/navbar/Navbar'
+
+import Button from '@/components/ui/Button'
+
+const MarketPlace = () => {
+  return (
+    <FrontendLayout>
+      <Navbar 
+        variant={ 'solid' }
+      />
+    </FrontendLayout>
+  )
+}
+
+export default MarketPlace
