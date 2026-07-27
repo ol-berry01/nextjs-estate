bef8dd900cc160ce3cacd2b22245da04580fd3fa
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-27 11:15:13 +0100

    feat: set initial card markup
    include link to future property listing page, property image and status

--- src/components/ui/PropertyCard.tsx
+++ src/components/ui/PropertyCard.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 import Property from '@/types/property'
 
 interface PropertyCardProps {
@@ -6,12 +9,33 @@ interface PropertyCardProps {
 
 const PropertyCard = ( { property }:PropertyCardProps ) => {
   return (
-    <p 
-      className={ '' }
+    <Link
+      href={ `/property/${ property.id }`}
+      className= { 'h-125 rounded-4xl relative group overflow-hidden'}
       key={ property.id }
     >
-      { property.title }
-    </p>
+      <div className="w-full h-full relative">
+        <Image
+          src={ property.image }
+          alt={ property.title }
+          fill
+          className={ 'object-cover inset-0 transition duration-700 group-hocer:scale-110' }
+        />
+
+        {/* dark overlay */}
+        <div className="bg-linear-to-t from-black/90 via-black/20 to-transparent absolute inset-0">
+          {/* top badge */}
+          <div className="bg-white/80 text-primary px-4 py-2 rounded-full text-sm font-semibold absolute top-5 left-5 z-20">
+            { property.status === 'rent' ? 'To let' : 'For sale' }
+          </div>
+        </div>
+      </div>
+      <p 
+        className={ '' }
+      >
+        { property.title }
+      </p>
+    </Link>
   )
 }
 
