22109b99d4b05006c502004317bc9a5a989c815c
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-28 09:32:42 +0100

    feat: add FrontendLayout, Navbar and initial header markup

--- src/app/property/[propertyId]/page.tsx
+++ src/app/property/[propertyId]/page.tsx
@@ -1,6 +1,22 @@
+import FrontendLayout from '@/components/layouts/FrontendLayout'
+
+import { Navbar } from '@/components/navbar/Navbar'
+
 const SingleProperty = () => {
   return (
-    <div>SingleProperty</div>
+    <FrontendLayout>
+      <Navbar
+        variant={ 'solid' }
+      />
+
+      <section className={ 'py-15' }>
+        <div className={ 'max-w-7xl mx-auto px-6 lg:px-12' }>
+          <div className={ 'flex flex-col gap-8 lg:flex-row lg:justify-between lg:items-start' }>
+
+          </div>
+        </div>
+      </section>
+    </FrontendLayout>
   )
 }
 
