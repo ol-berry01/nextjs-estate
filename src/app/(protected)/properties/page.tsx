b30b1f77f799b485287d3909d5e742da3df83407
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-28 09:13:36 +0100

    feat: duplicate markup from marketplace/page.tsx
    include imports but don't add 'filter' button

--- src/app/(protected)/properties/page.tsx
+++ src/app/(protected)/properties/page.tsx
@@ -1,6 +1,37 @@
+import FrontendLayout from '@/components/layouts/FrontendLayout'
+import { Navbar } from '@/components/navbar/Navbar'
+
+import PropertyCard from '@/components/ui/PropertyCard'
+import Button from '@/components/ui/Button'
+
+import properties from '@/constants/dummyProperties'
+
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+
 const PropertiesPage = () => {
   return (
-    <div>PropertiesPage</div>
+    <FrontendLayout>
+      <Navbar 
+        variant={ 'solid' }
+      />
+
+      <div className="w-full max-w-7xl mx-auto p-6 lg:px-12">
+        <div className="flex justify-between">
+          <h2 className="text-text text-2xl font-bold md:text-3xl">
+            Explore
+          </h2>
+        </div>
+
+        <div className={ 'my-4 grid md:grid-cols-2 xl:grid-cols-3 gap-8' }>
+          { properties.map( ( property ) => (
+            <PropertyCard 
+              key={ property.id }
+             property={ property }
+            />
+          ) )}
+        </div>
+      </div>
+    </FrontendLayout>
   )
 }
 
