9089387686ca569bfd0a9552c30e069d74714ee6
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-28 10:47:51 +0100

    feat: set prop types for Input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,4 +1,19 @@
-const Input = () => {
+import { InputHTMLAttributes } from 'react'
+
+interface BaseProps {
+  label: string,
+  name: string,
+  error?: string,
+  as?: 'input' | 'textarea',
+  value: string,
+  onchange?: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
+}
+
+type InputProps = BaseProps & InputHTMLAttributes<HTMLInputElement>
+
+type TextareaProps = BaseProps & InputHTMLAttributes<HTMLTextAreaElement>
+
+const Input = ( {}:InputProps | TextareaProps ) => {
   return (
     <div>Input</div>
   )
