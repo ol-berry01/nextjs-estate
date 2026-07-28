de4b78986208366ba7dd75f8d480acfa94807b9c
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-28 14:26:09 +0100

    feat: add body scroll lock to Modal component
    prevents the underlying page content from scrolling when a modal is open, enhancing user focus on the modals content

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+
 interface ModalProps {
   isOpen: boolean,
   onClose: () => void,
@@ -6,6 +8,13 @@ interface ModalProps {
 }
 
 const Modal = ( { isOpen, onClose, title, children } : ModalProps ) => {
+  useEffect( () => {
+    document.body.style.overflow = isOpen ? 'hidden' : 'auto'
+
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  }, [ isOpen ] )
   return (
     <div>Modal</div>
   )
