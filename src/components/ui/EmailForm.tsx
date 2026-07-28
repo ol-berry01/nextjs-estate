a19239cdc4b129369cd23890cab617d2a3d3e44d
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-28 10:35:56 +0100

    feat: add agent details to the top of the form

--- src/components/ui/EmailForm.tsx
+++ src/components/ui/EmailForm.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { useState } from 'react'
+import Image from 'next/image'
 
 interface InputValues {
   email: string,
@@ -21,11 +24,33 @@ const EmailForm = () => {
     setValues( ( prev ) => ( {
       ...prev,
       [ name ]:value
-    }) )
+    } ) )
   }
 
   return (
-    <div>EmailForm</div>
+    <div>
+      <div className={ 'bg-card p-8 border border-black/5 rounded-4xl shadow-sm sticky top-28' }>
+        <div className={ 'flex items-center gap-4' }>
+          <Image 
+            src={ '/images/avatar.png' }
+            alt={ 'User' }
+            width={ 50 }
+            height={ 50 }
+            className={ 'object-cover rounded-full' }
+          />
+
+          <div>
+            <h3 className={ 'text-text text-xl font-bold' }>
+              Sarah Johnson
+            </h3>
+
+            <p className={ 'text-text/60' }>
+              Property agent
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
