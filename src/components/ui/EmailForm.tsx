4386a69f146feab296a7bcd5210d2cbe755c845d
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-07-28 10:23:11 +0100

    feat: add values state
    set InputValues interface and default values

--- src/components/ui/EmailForm.tsx
+++ src/components/ui/EmailForm.tsx
@@ -1,4 +1,20 @@
+import { useState } from 'react'
+
+interface InputValues {
+  email: string,
+  name: string,
+  phone: string,
+  message: string
+}
+
 const EmailForm = () => {
+  const [ values, setValues ] = useState<InputValues>( {
+    email: '',
+    name: '',
+    phone: '',
+    message: ''
+  } )
+
   return (
     <div>EmailForm</div>
   )
