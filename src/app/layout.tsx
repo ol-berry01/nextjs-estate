f09d17bd5176b909aa2b682b258422ff53f97344
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-03 11:03:56 +0100

    feat: set isOpen and onClose Modal attributes
    import useAuthModal into RegisterModal and use openLogin, IsRegisterOpen and closeRegister properties
    
    add RegisterModal to layout.tsx for use by register button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ const poppins = Poppins( {
   weight: [ '300', '400', '500', '600', '700', '800' ]
 } )
 
+import RegisterModal from '@/components/modals/RegisterModal'
+
 export const metadata: Metadata = {
   title: 'Real estate marketplace',
   description: 'Estate marketplace built with Next.js, Tailwind CSS and TypeScript',
@@ -23,7 +25,12 @@ const RootLayout = ( {
       lang={ 'en' }
       className={ `${ poppins.className } h-full antialiased` }
     >
-      <body className={ 'bg-background min-h-full flex flex-col' }>{ children }</body>
+      <body 
+        className={ 'bg-background min-h-full flex flex-col' }
+      >
+        { children }
+        <RegisterModal />
+      </body>
     </html>
   )
 }
--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -1,10 +1,13 @@
 import Modal from './Modal'
+import useAuthModal from '@/store/useAuthModalStore'
 
 const RegisterModal = () => {
+  const { openLogin, isRegisterOpen, closeRegister } = useAuthModal()
+
   return (
     <Modal
-      isOpen={ false }
-      onClose={ false }
+      isOpen={ isRegisterOpen }
+      onClose={ closeRegister }
       title={ 'Register' }
     >
       <p>Register modal</p>
