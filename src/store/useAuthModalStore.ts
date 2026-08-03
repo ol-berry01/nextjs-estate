408ddd32903a4411ca90d9b199a94dd35bd661be
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-03 10:53:19 +0100

    feat: add closeAll rules
    refactor to properly layout the openLogic rules

--- src/store/useAuthModalStore.ts
+++ src/store/useAuthModalStore.ts
@@ -12,30 +12,36 @@ interface AuthModalStore {
 }
 
 const useAuthModal = create<AuthModalStore>( ( set ) => ( {
-  isLoginOpen: false
-  isRegisterOpen: false
+  isLoginOpen: false,
+  isRegisterOpen: false,
 
   openLogin: () => 
     set( {
       isLoginOpen: true,
       isRegisterOpen: false
-    } )
+    } ),
 
   closeLogin: () =>
     set( {
       isLoginOpen: false,
-    })
+    }),
 
   openRegister: () =>
     set( {
       isRegisterOpen: true,
       isLoginOpen: false
-    })
+    } ),
 
   closeRegister: () =>
     set( {
       isRegisterOpen: false
-    } )
+    } ),
+
+    closeAll: () => 
+      set( {
+        isLoginOpen: false,
+        isRegisterOpen: false
+      } )
   } ) 
 )
 
