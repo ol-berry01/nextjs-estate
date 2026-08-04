441ddf25e2ef817b157189cf877405f554950441
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-04 13:35:48 +0100

    feat: add CreatePropertyModal Zustand store
    Manages the isOpen state as well as providing open and close actions for the CreatePropertyModal

---
+++ src/store/useCreatePropertyModalStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+
+interface CreatePropertyModalStore {
+  isOpen: boolean,
+  open: () => void,
+  close: () => void
+}
+
+const useCreatePropertyModalStore = create<CreatePropertyModalStore>( ( set ) => ( {
+  isOpen: false,
+  open: () => set( { isOpen: true } ),
+  close: () => set( { isOpen: false } )
+} ) )
