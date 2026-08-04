8339531667cb9e5f093d9eab4290dcf1cc31f1af
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-04 10:05:33 +0100

    feat: add initial state for register form

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -1,10 +1,27 @@
 'use client'
 
+import { useState } from 'react'
+
 import Modal from './Modal'
 import useAuthModal from '@/store/useAuthModalStore'
 
+interface RegisterValues {
+  name: string,
+  email: string,
+  password: string
+}
+
+type RegisterErrors = Partial<Record<keyof RegisterValues, string>>
+
 const RegisterModal = () => {
   const { openLogin, isRegisterOpen, closeRegister } = useAuthModal()
+  const [ loading, setLoading ] = useState( false )
+  const [ values, setValues ] = useState<RegisterValues>( {
+    name: '',
+    email: '',
+    password: '',
+  } )
+  const [ errors, setErrors ] = useState<RegisterErrors>( {} )
 
   return (
     <Modal
