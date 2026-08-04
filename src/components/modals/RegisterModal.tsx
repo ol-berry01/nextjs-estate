6c4796e3ea5b1d97399e2b732b123368ac69c124
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-04 10:10:58 +0100

    feat: add handleChange and validate fuctions
    duplicated from LoginModal with adjustments for register form, including validation for name field

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -23,6 +23,48 @@ const RegisterModal = () => {
   } )
   const [ errors, setErrors ] = useState<RegisterErrors>( {} )
 
+  const handleChange = ( e:React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target
+
+    setValues( ( prev ) => ( {
+      ...prev,
+      [ name ]:value
+    } ) )
+
+    setErrors( ( prev ) => ( {
+      ...prev,
+      [ name ]: undefined
+    } ) )
+  }
+
+  const validate = () => {
+    const newErrors: RegisterErrors = {}
+
+    // validate the name
+    if ( ! values.name.trim() ) {
+      newErrors.name = 'Name is required'
+    } else if ( values.name.length < 2 ) {
+      newErrors.name = 'Enter a valid name with atleast 2 characters'
+    }
+    
+    // validate the email
+    if ( ! values.email.trim() ) {
+      newErrors.email = 'Email is required'
+    } else if ( ! /^\S+@\S+\.\S+$/.test( values.email ) ) {
+      newErrors.email = 'Enter a valid email address'
+    }
+
+    // validate the password
+    if ( ! values.password.trim() ) {
+      newErrors.password = 'Password is required'
+    } else if ( values.password.length < 6 ) {
+      newErrors.password = 'Enter a valid password with at least 6 characters'
+    }
+
+    setErrors( newErrors )
+    return Object.keys( newErrors ).length === 0
+  }
+
   return (
     <Modal
       isOpen={ isRegisterOpen }
