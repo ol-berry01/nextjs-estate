5db1fe0734fd69acbe7991ff43d379dce0b756d9
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-04 11:40:14 +0100

    feat: add 'continue with google' button

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -7,6 +7,8 @@ import Button from '../ui/Button'
 import Modal from './Modal'
 import useAuthModal from '@/store/useAuthModalStore'
 
+import { FcGoogle } from 'react-icons/fc'
+
 interface RegisterValues {
   name: string,
   email: string,
@@ -119,6 +121,27 @@ const RegisterModal = () => {
           Continue
         </Button>
       </form>
+
+      {/* divider */}
+      <div className={ 'my-6 relative'}>
+        <div className={ 'absolute inset-0 flex items-center'}>
+          <div className={ 'w-full border-t border-gray-300' } />
+        </div>
+        <div className="text-xs uppercase relative flex justify-center">
+          <span className="bg-white text-gray-500 px-4">
+            Or
+          </span>
+        </div>
+      </div>
+      
+      <Button
+        variant={ 'outline' }
+        icon={ <FcGoogle size={ 22 } />}
+        disabled={ loading }
+        fullWidth={ true }
+      >
+        Continue with Google
+      </Button>
     </Modal>
   )
 }
