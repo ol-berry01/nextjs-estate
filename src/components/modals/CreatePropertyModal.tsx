b5fb5f405d7e9f6cb6e81a311a30d3525654a51f
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-05 11:27:11 +0100

    feat: set modal steps

--- src/components/modals/CreatePropertyModal.tsx
+++ src/components/modals/CreatePropertyModal.tsx
@@ -1,10 +1,22 @@
 'use client'
 
+import { useState } from 'react'
+
 import useCreatePropertyModalStore from '@/store/useCreatePropertyModalStore'
 
 import Modal from './Modal'
 
+const STEPS = {
+  TYPE: 0,
+  LOCATION: 1,
+  DETAILS: 2,
+  FEATURES: 3,
+  IMAGE: 4,
+  PRICING: 5
+}
+
 const CreatePropertyModal = () => {
+  const [ steps, setSteps ] = useState( STEPS.TYPE )
   const { isOpen, close } = useCreatePropertyModalStore()
 
   return (
