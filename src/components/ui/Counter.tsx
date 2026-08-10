f3f89e171f7caa983648bfb4764d79f40f279114
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-10 15:00:20 +0100

    feat: add subTitle and plus & minus buttons
    conditionally display subTitle buttons linked to increase and decrease functions

--- src/components/ui/Counter.tsx
+++ src/components/ui/Counter.tsx
@@ -1,3 +1,5 @@
+import { LuMinus, LuPlus } from 'react-icons/lu'
+
 interface CounterProps {
   title: string,
   subTitle?: string,
@@ -16,7 +18,32 @@ const Counter = ( { title, subTitle, value, onChange, min = 1, max = 20 }:Counte
   }
 
   return (
-    <div>Counter</div>
+    <div className={ 'py-8 border-b flex justify-between items-center gap-8 last:border-b-0' }>
+      <div className={ '' }>
+        <p className={ 'text-gray-900 font-medium' }>
+          {title}
+        </p>
+        { subTitle && <p className={ 'text-gray-500 text-sm' }>{ subTitle }</p> }
+      </div>
+
+      <div className={ 'flex items-center gap-4' }>
+        <button
+          onClick={ decrease }
+          disabled={ min === value }
+          className={ 'w-8 h-8 border border-gray-300 rounded-full transition flex justify-center items-center hover:border-black disabled:opacity-30 disabled:cursor-not-allowed' }
+        >
+          <LuMinus size={ 16 } />
+        </button>
+        
+        <button
+          onClick={ increase }
+          disabled={ max === value }
+          className={ 'w-8 h-8 border border-gray-300 rounded-full transition flex justify-center items-center hover:border-black disabled:opacity-30 disabled:cursor-not-allowed' }
+        >
+          <LuPlus size={ 16 } />
+        </button>
+      </div>
+    </div>
   )
 }
 
