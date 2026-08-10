663a6bebd2077f84df0f859d0dedbf001429973a
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-10 14:30:54 +0100

    feat: set Counter props

--- src/components/ui/Counter.tsx
+++ src/components/ui/Counter.tsx
@@ -1,4 +1,13 @@
-const Counter = () => {
+interface CounterProps {
+  title: string,
+  subTitle?: string,
+  value: number,
+  onChange: ( value: number ) => void,
+  min?: number,
+  max?: number
+}
+
+const Counter = ( { title, subTitle, value, onChange, min = 1, max = 20 }:CounterProps ) => {
   return (
     <div>Counter</div>
   )
