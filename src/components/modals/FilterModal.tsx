a7deab9ec0912d8675511de3fdb941d224c4c788
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-11 12:44:22 +0100

    feat: create FilterModal

---
+++ src/components/modals/FilterModal.tsx
@@ -0,0 +1,13 @@
+import Modal from './Modal'
+
+const FilterModal = () => {
+  return (
+    <Modal
+      title={ 'Filter properties' }
+    >
+      FilterModal
+    </Modal>
+  )
+}
+
+export default FilterModal
