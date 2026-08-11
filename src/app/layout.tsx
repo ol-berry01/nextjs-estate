afdf6023c7b87e0c41ba0c320773cf5c32e24c56
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-11 13:09:59 +0100

    feat: set close and isOpen from useFilterModalStore
    import FilterModal into layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ const poppins = Poppins( {
 import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
 import CreatePropertyModal from '@/components/modals/CreatePropertyModal'
+import FilterModal from '@/components/modals/FilterModal'
 
 export const metadata: Metadata = {
   title: 'Real estate marketplace',
--- src/components/modals/FilterModal.tsx
+++ src/components/modals/FilterModal.tsx
@@ -1,9 +1,15 @@
+import useFilterModalStore from '@/store/useFilterModalStore'
+
 import Modal from './Modal'
 
 const FilterModal = () => {
+  const { close, isOpen } = useFilterModalStore()
+
   return (
     <Modal
       title={ 'Filter properties' }
+      onClose={ close }
+      isOpen={ isOpen }
     >
       FilterModal
     </Modal>
