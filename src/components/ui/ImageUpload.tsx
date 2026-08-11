19d3b2a1bb26d6df27bf153629dbc360a8b75bfa
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-11 09:44:15 +0100

    feat: set component props and handleUpload function

--- src/components/ui/ImageUpload.tsx
+++ src/components/ui/ImageUpload.tsx
@@ -1,4 +1,13 @@
+interface ImageUploadProps {
+  preview: string | null,
+  onChange: ( file:File ) => void
+}
+
 const ImageUpload = () => {
+  const handleUpload = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+    const file = e.target.files?.[0]
+  }
+
   return (
     <div>ImageUpload</div>
   )
