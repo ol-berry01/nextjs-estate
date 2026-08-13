14ff90d22d7b71752d47b7353d572b7501594f72
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-13 10:22:12 +0100

    feat: set filter modal steps and state

--- src/components/modals/FilterModal.tsx
+++ src/components/modals/FilterModal.tsx
@@ -1,11 +1,41 @@
 'use client'
 
+import { useState } from 'react'
 import useFilterModalStore from '@/store/useFilterModalStore'
 
 import Modal from './Modal'
 
+const STEPS = {
+  TYPE: 0,
+  LOCATION: 1,
+  PRICING: 2
+}
+
 const FilterModal = () => {
   const { close, isOpen } = useFilterModalStore()
+  
+  const [ step, setStep ] = useState( STEPS.TYPE )
+  const [ propertyType, setPropertyType ] = useState( '' )
+  const [ location, setLocation ] = useState( '' )
+  const [ address, setAddress ] = useState( '' )
+  const [ minPrice, setMinPrice ] = useState( '' )
+  const [ maxPrice, setMaxPrice ] = useState( '' )
+
+  const stepTitle = () => {
+    switch ( step ) {
+      case STEPS.TYPE:
+        return 'Select property type'
+      
+      case STEPS.LOCATION:
+        return 'Select property location'
+      
+      case STEPS.PRICING:
+        return 'Set property price range'
+
+      default:
+        return ''
+    }
+  }
 
   return (
     <Modal
@@ -13,7 +43,15 @@ const FilterModal = () => {
       onClose={ close }
       isOpen={ isOpen }
     >
-      FilterModal
+      {/* filter modal: header */}
+      <div className={ 'text-gray-500 mb-6 text-sm flex justify-between items-center' }>
+        <span className={ 'text-primary font-medium' }>
+          { stepTitle() }
+        </span>
+        <span>
+            Step { step + 1 } of { Object.keys( STEPS ).length }
+        </span>
+      </div>
     </Modal>
   )
 }
