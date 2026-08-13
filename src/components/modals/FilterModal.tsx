73479d3f0382a6b3c9eeb7663f4005ea259c0158
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-13 10:46:35 +0100

    feat: duplicate STEPS.TYPE from CreatePropertyModal

--- src/components/modals/FilterModal.tsx
+++ src/components/modals/FilterModal.tsx
@@ -3,7 +3,10 @@
 import { useState } from 'react'
 import useFilterModalStore from '@/store/useFilterModalStore'
 
+import propertyTypes from '@/constants/PropertyTypes'
+
 import Modal from './Modal'
+import PropertyTypeCard from '../ui/PropertyTypeCard'
 
 const STEPS = {
   TYPE: 0,
@@ -51,6 +54,23 @@ const FilterModal = () => {
         <span>
             Step { step + 1 } of { Object.keys( STEPS ).length }
         </span>
+      </div>
+
+      {/* filter modal: body */}
+      <div className={ 'text-gray-400 min-h-55 p-6 border boder-dashed border-gray-300 rounded-xl' }>
+        { step === STEPS.TYPE && (
+          <div className={ 'w-full max-h-[50vh] overflow-y-scoll no-scrollbar grid grid-cols-2 gap-4' }>
+            { propertyTypes.map( ( item ) => (
+              <PropertyTypeCard
+                key={ item.slug }
+                label={ item.label }
+                icon={ item.icon }
+                selected={ propertyType === item.slug }
+                onClick={ () => setPropertyType( item.slug ) }
+              />
+            ) ) }
+          </div>
+        ) }
       </div>
     </Modal>
   )
