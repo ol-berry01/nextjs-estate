77968f73c43ac5e6858e5912d839fef50d976d50
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-20 11:17:36 +0100

    feat: fetch current user into protected layout.tsx

--- src/app/(protected)/properties/layout.tsx
+++ src/app/(protected)/properties/layout.tsx
@@ -1,8 +1,11 @@
-const ProtectedLayout = ( {
+import getCurrentUser from '@/server-actions/getCurrentUser'
+
+const ProtectedLayout = async ( {
   children,
 }: Readonly<{
   children: React.ReactNode
 }> ) => {
+  await getCurrentUser()
   return (
     <>
       { children }
