82dae3aa6f4c0489dbdf0498c3dc98ca9ee11100
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-20 12:30:42 +0100

    feat: create variables from CreatePropertyModal

--- src/app/api/properties/route.ts
+++ src/app/api/properties/route.ts
@@ -16,6 +16,21 @@ const POST = async ( req: NextRequest ) => {
         }
       )
     }
+
+    const formData = await req.formData()
+
+    const title = formData.get( 'title' ) as string
+    const price = formData.get( 'price' ) as string
+    const description = formData.get( 'description' ) as string
+    const propertyType = formData.get( 'propertyType' ) as string
+    const listingType = formData.get( 'listingType' ) as string
+    const bedrooms = formData.get( 'bedrooms' ) as string
+    const bathrooms = formData.get( 'bathrooms' ) as string
+    const parkingSpaces = formData.get( 'parkingSpaces' ) as string
+    const location = formData.get( 'location' ) as string
+    const address = formData.get( 'address' ) as string
+    const area = formData.get( 'area' ) as string
+    const image = formData.get( 'image' ) as File
   } catch (error) {
     console.log( error )
 
