8b56efad51bf6a82b609b20ca4090bdbd17dfb2f
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-20 10:27:50 +0100

    feat: set await signIn.social for google provider

--- src/services/signInWithGoogle.ts
+++ src/services/signInWithGoogle.ts
@@ -1,8 +1,11 @@
+import { authClient } from '@/lib/auth-client'
 import { toast } from 'react-hot-toast'
 
 const signInWithGoogle = async () => {
   try {
-    
+    await authClient.signIn.social( {
+      provider: 'google'
+    })
   } catch ( error ) {
     toast.error( 'Google sign in failed' )
     console.log( error )
