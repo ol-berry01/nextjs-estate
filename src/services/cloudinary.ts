f9beb8f44352dec6c79eab130be03de37cd38331
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-21 12:40:34 +0100

    feat: declare cloudinary variables

--- src/services/cloudinary.ts
+++ src/services/cloudinary.ts
@@ -5,6 +5,17 @@ export type CloudinaryUploadResult = {
   public_id: string
 }
 
-const uploadToCloudinary = async ( file: File ): Promise<CloudinaryUploadResult> => {}
+const uploadToCloudinary = async ( file: File ): Promise<CloudinaryUploadResult> => {
+  const bytes = await file.arrayBuffer()
+  const buffer = Buffer.from( bytes )
+  const base64 = buffer.toString( 'base64' )
+  const dataUrl = `data:${ file.type };base64,${ base64 }`
+
+  try {
+    
+  } catch (error) {
+    
+  }
+}
 
 export default uploadToCloudinary
