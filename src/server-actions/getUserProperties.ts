9cc36eb84877fb2bcd916fd9084f765a18dac417
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-25 10:25:11 +0100

    feat: get properties list for current user
    include check for logged in user

--- src/server-actions/getUserProperties.ts
+++ src/server-actions/getUserProperties.ts
@@ -1,6 +1,17 @@
+import { prisma } from '@/database/db'
+import getCurrentUser from './getCurrentUser'
+
 const getUserProperties = async () => {
   try {
-    
+    const currentUser = await getCurrentUser()
+
+    if ( ! currentUser?.id ) {
+      return []
+    }
+
+    const properties = await prisma.property.findMany( {
+      where: { ownerId: currentUser.id }
+    } )
   } catch ( error ) {
     console.error( 'Failed to fetch properties', error )
     return []
