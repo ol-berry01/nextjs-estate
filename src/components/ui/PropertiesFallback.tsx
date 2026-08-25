7fffd6963d0f76d317bf855dffb506cbaad1f419
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-25 14:01:09 +0100

    feat: set component interface

--- src/components/ui/PropertiesFallback.tsx
+++ src/components/ui/PropertiesFallback.tsx
@@ -1,4 +1,10 @@
-const PropertiesFallback = () => {
+interface PropertiesFallbackProps {
+  title: string,
+  subTitle?: string,
+  filter?: boolean
+}
+
+const PropertiesFallback = ( { title, subTitle, filter }: PropertiesFallbackProps ) => {
   return (
     <div className={ 'w-full h-[50vh] my-10 border border-black/5 rounded-xl flex justify-center items-center' }>
       <p className={ 'text-text' }>
