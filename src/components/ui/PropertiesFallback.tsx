aadca5c3581d172180c16080435e963cf28d0253
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-25 13:59:50 +0100

    feat: create PropertiesFallback as its own component

---
+++ src/components/ui/PropertiesFallback.tsx
@@ -0,0 +1,11 @@
+const PropertiesFallback = () => {
+  return (
+    <div className={ 'w-full h-[50vh] my-10 border border-black/5 rounded-xl flex justify-center items-center' }>
+      <p className={ 'text-text' }>
+        No properties found
+      </p>
+    </div>
+  )
+}
+
+export default PropertiesFallback
