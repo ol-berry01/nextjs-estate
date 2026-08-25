ef3bd1560a34f495ceee4f209cd0e8554fde8bd4
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-25 14:08:22 +0100

    feat: add HiOutlineHomeModern react-icon

--- src/components/ui/PropertiesFallback.tsx
+++ src/components/ui/PropertiesFallback.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import { useRouter } from 'next/navigation'
+
+import { HiOutlineHomeModern } from 'react-icons/hi2'
 
 interface PropertiesFallbackProps {
   title: string,
@@ -9,7 +13,11 @@ interface PropertiesFallbackProps {
 const PropertiesFallback = ( { title, subTitle, filter }: PropertiesFallbackProps ) => {
   const router = useRouter()
   return (
-    <div className={ 'w-full h-[50vh] my-10 border border-black/5 rounded-xl flex justify-center items-center' }>
+    <div className={ 'w-full h-[50vh] my-10 border border-black/5 rounded-xl flex flex-col justify-center items-center' }>
+      <HiOutlineHomeModern
+        size={ 40 }
+        className={ 'text-primary' }
+      />
       <p className={ 'text-text' }>
         No properties found
       </p>
