f7a76845506c388a493a4f66201938f47cf28545
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-25 12:43:38 +0100

    feat: set skeleton content

--- src/components/ui/CardSkeleton.tsx
+++ src/components/ui/CardSkeleton.tsx
@@ -1,6 +1,13 @@
 const CardSkeleton = () => {
   return (
-    <div>CardSkeleton</div>
+    <div className={ 'my-6 grid gap-8 md:grid-cols-2 xl:grid-cols-3'}>
+      { Array.from( { length: 6 } ).map( ( _, index ) => (
+        <div 
+          key={ index }
+          className={ 'bg-black h-105 rounded-3xl animate-pulse' } 
+        />
+      ) ) }
+    </div>
   )
 }
 
