3e875f1e11d85fc496069f54a8ec737a58ec707c
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-26 08:46:00 +0100

    feat: create properties variable from prisma.property
    set function to be async

--- src/server-actions/getRecentProperties.ts
+++ src/server-actions/getRecentProperties.ts
@@ -1,6 +1,9 @@
-const getRecentProperties = () => {
-  try {
 
+import { prisma } from '@/database/db'
+
+const getRecentProperties = async () => {
+  try {
+    const properties = await prisma.property.findMany( {} )
   } catch ( error ) {
     console.error( 'Failed to get latest properties' )
     return []
