760e2b903231fe5409b47efffc8ec9b0f3553bf4
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-26 08:46:59 +0100

    feat: set to return 6 most recent properties

--- src/server-actions/getRecentProperties.ts
+++ src/server-actions/getRecentProperties.ts
@@ -3,7 +3,14 @@ import { prisma } from '@/database/db'
 
 const getRecentProperties = async () => {
   try {
-    const properties = await prisma.property.findMany( {} )
+    const properties = await prisma.property.findMany( {
+      take: 6,
+      orderBy: {
+        createdAt: 'desc'
+      }
+    } )
+
+    return properties
   } catch ( error ) {
     console.error( 'Failed to get latest properties' )
     return []
