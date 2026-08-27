3576fee90ad8a89a43761713208b04877e02d5da
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-27 14:55:23 +0100

    feat: create getProperty server action
    set error handling

---
+++ src/server-actions/getProperty.ts
@@ -0,0 +1,10 @@
+const getProperty = async ( propertyId: string ) => {
+  try {
+
+  } catch ( error ) {
+    console.error( 'Failed to fetch property:', error )
+    return null
+  }
+}
+
+export default getProperty
