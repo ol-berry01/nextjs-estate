9560652bae219e12a720a3049961498d47070a21
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-27 10:22:14 +0100

    feat: rename component to avoid conflict

--- src/components/marketplace/MarketplaceItems.tsx
+++ src/components/marketplace/MarketplaceItems.tsx
@@ -2,7 +2,7 @@ import PropertyCard from '@/components/ui/PropertyCard'
 
 import properties from '@/constants/dummyProperties'
 
-const Marketplace = () => {
+const MarketplaceItems = () => {
   return (
     <div className={ 'my-4 grid md:grid-cols-2 xl:grid-cols-3 gap-8' }>
       { properties.map( ( property ) => (
@@ -15,4 +15,4 @@ const Marketplace = () => {
   )
 }
 
-export default Marketplace
+export default MarketplaceItems
