cb636fd58adddcf8499ebf3daec77f42df97257d
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-27 11:08:02 +0100

    feat: set component props

--- src/components/marketplace/MarketplaceItems.tsx
+++ src/components/marketplace/MarketplaceItems.tsx
@@ -1,8 +1,19 @@
+type MarketplaceItemsProps = {
+  searchParams: {
+    search?: string,
+    propertyType?: string,
+    location?: string,
+    address?: string,
+    minPrice?: number,
+    maxPrice?: number
+  }
+}
+
 import PropertyCard from '@/components/ui/PropertyCard'
 
 import properties from '@/constants/dummyProperties'
 
-const MarketplaceItems = () => {
+const MarketplaceItems = ( { searchParams }: MarketplaceItemsProps ) => {
   return (
     <div className={ 'my-4 grid md:grid-cols-2 xl:grid-cols-3 gap-8' }>
       { properties.map( ( property ) => (
