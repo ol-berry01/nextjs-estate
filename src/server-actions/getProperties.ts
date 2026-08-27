5c88cafa1344812fcdecc4b8f539d0e748c4cee5
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-27 10:02:18 +0100

    feat: add query parameter support and return properties data

--- src/server-actions/getProperties.ts
+++ src/server-actions/getProperties.ts
@@ -11,7 +11,21 @@ interface GetPropertiesParams {
 
 const getProperties = async ( params?: GetPropertiesParams ) => {
   try {
-    const {} = await axios.get( `${ process.env.NEXT_PUBLIC_BASE_URL }/api/properties` )
+    const { data } = await axios.get( 
+      `${ process.env.NEXT_PUBLIC_BASE_URL }/api/properties`,
+      {
+        params: {
+          search: params?.search,
+          propertyType: params?.propertyType,
+          location: params?.location,
+          address: params?.address,
+          minPrice: params?.minPrice,
+          maxPrice: params?.maxPrice
+        }
+      }
+    )
+
+    return data
   } catch (error) {
     throw new Error( 'Failed to fetch properties', { cause: error } )
   }
