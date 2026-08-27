fed9a1f791fd54fcbef79fbee03769249eb5cfd2
Oliver Berry <info@littlemonsterdigital.co.uk>
2026-08-27 15:15:37 +0100

    feat: return unique property
    handle if property doesn't exist

--- src/server-actions/getProperty.ts
+++ src/server-actions/getProperty.ts
@@ -1,6 +1,28 @@
+import { prisma } from '@/database/db'
+
 const getProperty = async ( propertyId: string ) => {
   try {
+    const property = await prisma.property.findUnique( {
+      where: {
+        id: propertyId
+      },
+      include: {
+        owner: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true
+          }
+        }
+      }
+    } )
 
+    if ( ! property ) {
+      return null
+    }
+
+    return property
   } catch ( error ) {
     console.error( 'Failed to fetch property:', error )
     return null
